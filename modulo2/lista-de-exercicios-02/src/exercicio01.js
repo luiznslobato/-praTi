065363173411b3565577e4b9ed1acbe475be6ed3
luiznslobato <contato.luiznonato@gmail.com>
2024-06-12 15:58:13 -0300

    Exercício 01 resolvido

--- modulo2/lista-de-exercicios-02/src/exercicio01.js
+++ modulo2/lista-de-exercicios-02/src/exercicio01.js
@@ -1,1 +1,16 @@
+let prompt = require("prompt-sync")();
 
+function diasDeVidaPerdidos() {
+    let tempoPerdidoPorCigarro = 10;
+    let diaEmMinutos = 1440;
+
+    let cigarrosPorDia = parseInt(prompt("Quantos cigarros você fuma por dia? "));
+    let anosFumando = parseInt(prompt("Quantos anos você já fumou? "));
+
+    let totalDeCigarros = cigarrosPorDia * 365 * anosFumando
+    let perdaDeVida = parseInt((tempoPerdidoPorCigarro * totalDeCigarros) / diaEmMinutos);
+
+    console.log("Voce já perdeu cerca de", perdaDeVida, "dias de vida.");
+}
+
+diasDeVidaPerdidos();
