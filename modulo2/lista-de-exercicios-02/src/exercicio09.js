bb0116762667124c9c7db3e431e4e67a4b14c74b
luiznslobato <contato.luiznonato@gmail.com>
2024-06-13 11:38:24 -0300

    Exercício 09 resolvido

---
+++ modulo2/lista-de-exercicios-02/src/exercicio09.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+
+function salarios() {
+
+    let salarioTotalHomens = 0;
+    let salarioTotalMulheres = 0;
+    let continuar = true;
+
+    while (continuar) {
+        let salario = parseFloat(prompt("Quanto é o seu salário: "));
+        let genero = prompt("Qual é o seu sexo: [H] Homem - [M] Mulher: ").toUpperCase();
+        continuar = prompt("Continuar adicionando mais salários: [S] Sim - [N] Não: ").toUpperCase() === "N"
+            ? false : true;
+
+        if (genero === "H") {
+            salarioTotalHomens += salario;
+        } else if (genero === "M") {
+            salarioTotalMulheres += salario;
+        } else {
+            console.log("Opção inválida");
+            continue;
+        }
+
+        console.log(`Salário total: Homens R$${salarioTotalHomens}; Mulheres R$${salarioTotalMulheres}.`)
+    }
+}
+salarios();
