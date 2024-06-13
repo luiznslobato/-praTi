21c9a78c6f56414fb92e09e8187a862059c3cade
luiznslobato <contato.luiznonato@gmail.com>
2024-06-12 22:43:14 -0300

    Exercício 05 melhorado

--- modulo2/lista-de-exercicios-02/src/exercicio05.js
+++ modulo2/lista-de-exercicios-02/src/exercicio05.js
@@ -2,22 +2,20 @@ let prompt = require("prompt-sync")();
 
 function jokenPo() {
     let jogadaDoUsuario = parseInt(prompt("Faça a sua jogada: [1] Pedra - [2] Papel - [3] Tesoura: "));
-    let jogadaDoComputador = Math.floor(Math.random() * 3)
+    let jogadaDoComputador = Math.floor(Math.random() * 3);
 
     if (jogadaDoComputador === jogadaDoUsuario) {
-        console.log("EMPATE, o computador jogou o mesmo que você!")
+        console.log("EMPATE, o computador jogou o mesmo que você!");
 
     } else if (jogadaDoComputador === 1) {
         if (jogadaDoUsuario === 2) {
             console.log("GANHOU, o computador jogou pedra!");
-
         } else {
             console.log("PERDEU, o computador jogou pedra!");
         }
     } else if (jogadaDoComputador === 2) {
         if (jogadaDoUsuario === 1) {
             console.log("PERDEU, o computador jogou papel!");
-
         } else {
             console.log("GANHOU, o computador jogou papel!");
         }
