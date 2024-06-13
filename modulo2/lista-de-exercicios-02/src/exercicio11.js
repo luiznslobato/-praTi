953df125e223494b226007e0403e4cea84e912fa
luiznslobato <contato.luiznonato@gmail.com>
2024-06-13 12:37:22 -0300

    Exercício 11 resolvido

---
+++ modulo2/lista-de-exercicios-02/src/exercicio11.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+function pa() {
+
+    let primeiroTermo = parseInt(prompt("Defina o primeiro termo da PA: "));
+    let razao = parseInt(prompt("Defina a razão da PA: "));
+    let soma = 0
+
+    for (let i = 0; i < 10; i++) {
+        soma += primeiroTermo + (i * razao);
+        console.log(`${primeiroTermo + (i * razao)}`);
+    }
+    console.log(`A soma dos termos da PA é de: ${soma}`);
+}
+
+pa()
