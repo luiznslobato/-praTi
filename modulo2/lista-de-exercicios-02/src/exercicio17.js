0b821cb58fa22fc557345b8e3de30a337225c483
luiznslobato <contato.luiznonato@gmail.com>
2024-06-14 10:06:52 -0300

    Exercício 17 resolvido

---
+++ modulo2/lista-de-exercicios-02/src/exercicio17.js
@@ -0,0 +1,27 @@
+let prompt = require("prompt-sync")();
+
+
+function menorIdade() {
+
+    let nomes = [];
+    let idades = [];
+
+    console.log("---- Informe o nome e a idade de nove pessoas -----")
+
+    for (let i = 0; i < 9; i++) {
+        let nome = (prompt("Nome: "));
+        let idade = parseInt(prompt("Idade: "));
+        nomes.push(nome);
+        idades.push(idade);
+    }
+
+    console.log("----------------- Menores de idade -----------------")
+
+    for (let i = 0; i < 9; i++) {
+        if (idades[i] < 18) {
+            console.log(`Nome: ${nomes[i]} - Idade: ${idades[i]} anos`);
+        }
+    }
+}
+
+menorIdade();
